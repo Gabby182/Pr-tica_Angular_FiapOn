4fd8c9fd08753979329c83d5358fd95150ae067d
Gabriele Pereira <g.alves.pereira@avanade.com>
2022-01-14 17:14:37 -0300

    "Atualizando: a aula do dia 1/14/2022"

--- fiap-social/src/app/views/shared/post/post.component.ts
+++ fiap-social/src/app/views/shared/post/post.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.css']
+  styleUrls: ['./post.component.css'],
 })
 export class PostComponent implements OnInit {
 
-  constructor() { }
+  @Input() post: any;
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  sendComment = () => {
+    alert("Enviando um comentário")
   }
-
 }
